3212c9c2e9aef41228cbc385a8efc92487707db3
3nvi <agg.arvanitaks@gmail.com>
2017-03-31 19:25:55 +0300

    changed router type

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Base from './components/Base';
 
 function Routes() {
   return (
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
       <Route path="/" component={Base}>
         <IndexRoute component={Home} />
         <Route path="/profile/:id/" component={Profile} />
